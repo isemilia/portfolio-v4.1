ee5a5b509bf9da10cb9eb66f8a0f1fab8a705dba
Emilia <81705094+isemilia@users.noreply.github.com>
2025-04-18 23:58:04 +0500

    Fetch projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,42 @@
 import Filters from '@/ui/components/filters';
 import Container from '@/ui/elements/container';
+import Project from '@/ui/components/project';
 
-const Page = () => {
+const Page = async () => {
+  const response = await fetch('http://localhost:3000/api/projects');
+  const projects = await response.json();
+
   const filters = [
     { name: 'static', label: 'Static' },
     { name: 'js', label: 'Plain JS' },
     { name: 'spa', label: 'SPA' },
   ];
 
+  console.log(projects);
+
+  // todo: type response, make filters work
+
   return (
     <Container>
       <Filters options={filters} defaultValue={'static'} />
-      <div className={'mt-[30px]'}>{/*<Project />*/}</div>
+      <div className={'mt-[30px]'}>
+        {projects.data.map((project) => {
+          return (
+            <Project
+              key={project.id}
+              href={project.href}
+              title={project.title}
+              description={project.description}
+              img={project.img}
+              links={project.links}
+              chips={project.tags.map((tag) => ({
+                color: tag.type,
+                label: tag.label,
+              }))}
+            />
+          );
+        })}
+      </div>
     </Container>
   );
 };
