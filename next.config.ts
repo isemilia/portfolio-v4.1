c7cf649ab57473dfc9af3d081f68a6165e83c055
Emilia <81705094+isemilia@users.noreply.github.com>
2025-05-20 07:54:58 +0500

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/api/projects': ['./data/projects.json'],
+    '/api/work-experience': ['./data/timeline.json'],
+  },
+};
 
 export default nextConfig;
